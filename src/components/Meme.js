578e3e8d825309d157660f11f093a6c0ed0de4af
Andres Garza <hanato21@icloud.com>
2023-10-27 17:16:59 -0500

    update

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,14 +1,16 @@
 import React from "react";
 import memesData from "../memesData.js";
 
-function getMemeImage() {
-  const memesArray = memesData.data.memes;
-  const randomNumber = Math.floor(Math.random() * memesArray.length);
-  const url = memesArray[randomNumber].url;
-  console.log(url);
-}
-
 export default function Meme() {
+  
+  const [memeImage, setMemeImage] = React.useState("")
+
+  function getMemeImage() {
+    const memesArray = memesData.data.memes;
+    const randomNumber = Math.floor(Math.random() * memesArray.length);
+    setMemeImage(memesArray[randomNumber].url);
+  }
+
   return (
     <main>
       <div className="form">
@@ -18,6 +20,7 @@ export default function Meme() {
           Get a new meme image
         </button>
       </div>
+      <img src={memeImage} />
     </main>
   );
 }
